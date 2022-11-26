f68f5c4f894397ac78cb32b194bb26f100e0891f
Leon <mail@leonbohmann.de>
2022-11-26 22:40:33 +0100

    update walltime to -t 00:15:00 (--time did not work)

--- script/bash.sh
+++ script/bash.sh
@@ -5,13 +5,15 @@
 #SBATCH --reservation=kurs00060
 #  #SBATCH --mail-type=ALL
 #  #SBATCH --mail-user=marcel.heis@tu-darmstadt.de
-#SBATCH -e /work/home/kurse/kurs00060/lb82noru/ue1/out/ue1/log/ue1.err.%j
-#SBATCH -o /work/home/kurse/kurs00060/lb82noru/ue1/out/ue1/log/ue1.out.%j
+#SBATCH -e /work/home/kurse/kurs00060/lb82noru/ue1/out/ue1.err.%j
+#SBATCH -o /work/home/kurse/kurs00060/lb82noru/ue1/out/ue1.out.%j
+#SBATCH -t 00:15:00
 
-#SBATCH --mem-per-cpu=1000
-#SBATCH --time=00:30
+#SBATCH --mem-per-cpu=30000
 #SBATCH -n 1
 #SBATCH --gres=gpu:a100:1
+
+echo "This is Job  - $SLURM_JOB_ID"
 
  # first remove all modules and load required modules
 module purge
@@ -19,4 +21,4 @@ module load java/16.0.1
 module load cuda
 
  # call to the parallel program
-srun java -jar HighPerformanceSimulation.jar
+srun java -jar HighPerformanceSimulation.jar -Xms20g
